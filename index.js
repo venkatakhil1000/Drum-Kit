6f1d65a2d9c83c7b579a444c1f9c64071b9d8724
Venkat Akhil <72782061+venkatakhil1000@users.noreply.github.com>
2020-11-05 00:15:32 +0530

    TK-Music

---
+++ index.js
@@ -0,0 +1,69 @@
+var numOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numOfDrumButtons; i++) {
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+
+        var buttonInnerHTML = this.innerHTML;
+
+        makeSound(buttonInnerHTML);
+
+        addAnimation(buttonInnerHTML);
+
+    });
+
+}
+
+document.addEventListener("keypress", function(event) {
+    makeSound(event.key);
+    addAnimation(event.key);
+});
+
+function makeSound(key) {
+
+
+    switch (key) {
+        case "w":
+            var tom1 = new Audio("tom-1.mp3");
+            tom1.play();
+            break;
+        case "s":
+            var tom2 = new Audio("tom-2.mp3");
+            tom2.play();
+            break;
+        case "a":
+            var tom3 = new Audio("tom-3.mp3");
+            tom3.play();
+            break;
+        case "d":
+            var tom4 = new Audio("tom-4.mp3");
+            tom4.play();
+            break;
+        case "j":
+            var snare = new Audio("snare.mp3");
+            snare.play();
+            break;
+        case "k":
+            var kick = new Audio("kick-bass.mp3");
+            kick.play();
+            break;
+        case "l":
+            var crash = new Audio("crash.mp3");
+            crash.play();
+            break;
+        default:
+            console.log(buttonInnerHTML);
+            break;
+    }
+}
+
+
+function addAnimation(currentKey) {
+
+    document.querySelector("." + currentKey).classList.add("pressed");
+
+    setTimeout(function() {
+        document.querySelector("." + currentKey).classList.remove("pressed");
+    }, 200);
+
+}
